0d24ccf2d78f51599f9effde9d2385bd1e1b11fd
Wallyson <wallysom2@gmail.com>
2022-01-27 16:11:53 -0300

    inclusao do js e finalizacao do html/css

---
+++ script.js
@@ -0,0 +1,46 @@
+function select11 (){
+    document.getElementById("caixa11").style.borderColor = "green"
+    document.getElementById("caixa12").style.borderColor = "white"
+    document.getElementById("caixa13").style.borderColor = "white"
+}
+function select12 (){
+    document.getElementById("caixa11").style.borderColor = "white"
+    document.getElementById("caixa12").style.borderColor = "green"
+    document.getElementById("caixa13").style.borderColor = "white"
+}
+function select13 (){
+    document.getElementById("caixa11").style.borderColor = "white"
+    document.getElementById("caixa12").style.borderColor = "white"
+    document.getElementById("caixa13").style.borderColor = "green"
+}
+function select21 (){
+    document.getElementById("caixa21").style.borderColor = "green"
+    document.getElementById("caixa22").style.borderColor = "white"
+    document.getElementById("caixa23").style.borderColor = "white"
+}
+function select22 (){
+    document.getElementById("caixa21").style.borderColor = "white"
+    document.getElementById("caixa22").style.borderColor = "green"
+    document.getElementById("caixa23").style.borderColor = "white"
+}
+function select23 (){
+    document.getElementById("caixa21").style.borderColor = "white"
+    document.getElementById("caixa22").style.borderColor = "white"
+    document.getElementById("caixa23").style.borderColor = "green"
+}
+function select31 (){
+    document.getElementById("caixa31").style.borderColor = "green"
+    document.getElementById("caixa32").style.borderColor = "white"
+    document.getElementById("caixa33").style.borderColor = "white"
+}
+function select32 (){
+    document.getElementById("caixa31").style.borderColor = "white"
+    document.getElementById("caixa32").style.borderColor = "green"
+    document.getElementById("caixa33").style.borderColor = "white"
+}
+function select33 (){
+    document.getElementById("caixa31").style.borderColor = "white"
+    document.getElementById("caixa32").style.borderColor = "white"
+    document.getElementById("caixa33").style.borderColor = "green"
+}
+
