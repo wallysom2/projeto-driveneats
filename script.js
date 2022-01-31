fd0527e8692d8fb1d6383bef195b3016ce7bc403
Wallyson <wallysom2@gmail.com>
2022-01-31 12:52:35 -0300

    Acabei a fase de marcar alimentos selecionados e alterrar o botao de finalizar pedido

--- script.js
+++ script.js
@@ -1,46 +1,63 @@
-function select11 (){
-    document.getElementById("caixa11").style.borderColor = "green"
-    document.getElementById("caixa12").style.borderColor = "white"
-    document.getElementById("caixa13").style.borderColor = "white"
-}
-function select12 (){
-    document.getElementById("caixa11").style.borderColor = "white"
-    document.getElementById("caixa12").style.borderColor = "green"
-    document.getElementById("caixa13").style.borderColor = "white"
-}
-function select13 (){
-    document.getElementById("caixa11").style.borderColor = "white"
-    document.getElementById("caixa12").style.borderColor = "white"
-    document.getElementById("caixa13").style.borderColor = "green"
-}
-function select21 (){
-    document.getElementById("caixa21").style.borderColor = "green"
-    document.getElementById("caixa22").style.borderColor = "white"
-    document.getElementById("caixa23").style.borderColor = "white"
-}
-function select22 (){
-    document.getElementById("caixa21").style.borderColor = "white"
-    document.getElementById("caixa22").style.borderColor = "green"
-    document.getElementById("caixa23").style.borderColor = "white"
-}
-function select23 (){
-    document.getElementById("caixa21").style.borderColor = "white"
-    document.getElementById("caixa22").style.borderColor = "white"
-    document.getElementById("caixa23").style.borderColor = "green"
-}
-function select31 (){
-    document.getElementById("caixa31").style.borderColor = "green"
-    document.getElementById("caixa32").style.borderColor = "white"
-    document.getElementById("caixa33").style.borderColor = "white"
-}
-function select32 (){
-    document.getElementById("caixa31").style.borderColor = "white"
-    document.getElementById("caixa32").style.borderColor = "green"
-    document.getElementById("caixa33").style.borderColor = "white"
-}
-function select33 (){
-    document.getElementById("caixa31").style.borderColor = "white"
-    document.getElementById("caixa32").style.borderColor = "white"
-    document.getElementById("caixa33").style.borderColor = "green"
+let pratoEsc = 0;
+let valorPratoEsc;
+let bebidaEsc = 0;
+let valorBebidaEsc;
+let doceEsc = 0;
+let valorDoceEsc;
+
+
+
+function selecionarPrato (prato, nomePrato , valorPrato){
+    desmarcarAPrato ()
+    const pratoSelecionado = document.querySelector ("." + prato)
+    pratoSelecionado.classList.add ("alimento-selecionado")
+
+    function desmarcarAPrato (){
+        const desmaAlimento = document.querySelector(".alimento-selecionado")
+        if (desmaAlimento !== null){
+        desmaAlimento.classList.remove("alimento-selecionado")
+        }
+    }
+    pratoEsc = nomePrato
+    valorPratoEsc = valorPrato
+    finalizarPedido ()
 }
 
+function selecionarBebida (bebida, nomeBebida , valorBebida){
+    desmarcarBebida ()
+    const bebidaSelecionado = document.querySelector ("." + bebida)
+    bebidaSelecionado.classList.add ("bebida-selecionada")
+
+    function desmarcarBebida (){
+        const desmaBebida = document.querySelector(".bebida-selecionada")
+        if (desmaBebida !== null){
+            desmaBebida.classList.remove("bebida-selecionada")
+        }
+    }
+    bebidaEsc = nomeBebida
+    valorBebidaEsc = valorBebida
+    finalizarPedido ()
+}
+function selecionarDoce (doce, nomeDoce, valorDoce){
+    desmarcarDoce()
+    const doceSelecionado = document.querySelector ("." + doce)
+    doceSelecionado.classList.add ("doce-selecionado")
+
+    function desmarcarDoce (){
+        const desmarcarDoce = document.querySelector(".doce-selecionado")
+        if (desmarcarDoce !== null){
+            desmarcarDoce.classList.remove("doce-selecionado")
+        }
+    }
+    doceEsc = nomeDoce
+    valorDoceEsc = valorDoce
+    finalizarPedido ()
+}
+function finalizarPedido () {
+    const termPedido = document.querySelector (".finalizar-pedido")
+    if (pratoEsc!== 0 && bebidaEsc !== 0 && doceEsc !== 0){
+        termPedido.classList.add ("botao-selecionado")
+        termPedido.innerText = "Finalizar Pedido"
+        termPedido.classList.add ("text-final")
+    }
+}
