c199cee78833fb6457b68f8e54009c187cfba2f8
Wallyson <wallysom2@gmail.com>
2022-01-31 16:51:46 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let pratoEsc = 0;
-let valorPratoEsc;
+let valorPratoEsc = 0;
 let bebidaEsc = 0;
-let valorBebidaEsc;
+let valorBebidaEsc = 0;
 let doceEsc = 0;
-let valorDoceEsc;
-
-
+let valorDoceEsc = 0;
+let valorTotal = 0;
 
 function selecionarPrato (prato, nomePrato , valorPrato){
     desmarcarAPrato ()
@@ -61,3 +60,39 @@ function finalizarPedido () {
         termPedido.classList.add ("text-final")
     }
 }
+function fazerPedidoClique (){
+    const fazerPedido = document.querySelector(".conf-pedido")
+    fazerPedido.classList.remove ("esconder")
+
+    const pratoConf = document.querySelector(".prato-conf")
+    pratoConf.innerHTML = (`${pratoEsc}`)
+    const bebidaConf = document.querySelector(".bebida-conf")
+    bebidaConf.innerHTML = (`${bebidaEsc}`)
+    const doceConf = document.querySelector(".doce-conf")
+    doceConf.innerHTML = (`${doceEsc}`)
+
+    const precoPratoConf = document.querySelector(".preco-prato-conf")
+    precoPratoConf.innerHTML = (`${valorPratoEsc}`)
+    const precoBebidaConf = document.querySelector(".preco-bebida-conf")
+    precoBebidaConf.innerHTML = (`${valorBebidaEsc}`)
+    const precoDoceConf = document.querySelector(".preco-doce-conf")
+    precoDoceConf.innerHTML = (`${valorDoceEsc}`)  
+    const precoTotalConf = document.querySelector(".preco-total-conf")
+    precoTotalConf.innerHTML = (`R$ ${valorDoceEsc+valorBebidaEsc+valorPratoEsc}`) 
+
+}
+function cancelarConf (){
+    const cancelarConfi = document.querySelector (".conf-pedido")
+    cancelarConfi.classList.add ("esconder")
+}
+function whatsapp(){
+    let mensagem;
+    mensagem = `Olá, gostaria de fazer o pedido:\n
+    - Prato: ${pratoEsc}\n
+    - Bebida: ${bebidaEsc}\n
+    - Sobremesa: ${doceEsc}\n
+    Total: R$ ${valorDoceEsc+valorBebidaEsc+valorPratoEsc}\n`
+
+    window.open("https://wa.me/5584981353185?text=" + mensagem)
+}
+
